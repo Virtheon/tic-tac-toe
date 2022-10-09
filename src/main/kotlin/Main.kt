0089805eaabeb717438ecbcb62e0b3081c6ba2be
Henrique Aguiar <henriquesarmentogoulartaguiar@gmail.com>
2022-10-09 19:50:09 +0100

    Added optimised win-checking function, added constructors to copy board effectively and cleaned up terminology

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,30 +4,20 @@ enum class Symbol(private val string: String) {
 	override fun toString(): String = string
 }
 
-class Board(val size: Int) {
+class Board private constructor(
+	val size: Int,
+	val winAxes: Array<Array<IntArray>>,
+	private val squares: Array<Array<Symbol>>
+) {
 	// size - 1 was used a lot in iterations, so having an index variable instead makes code less prone to mistakes
 	private val maxIndex = size - 1
-	private val squares = Array(size) { Array(size) { Symbol.EMPTY } }
 
+	// TODO clean up comments (again)
 	// Three-dimensional array that stores all axes in which a player can win.
-	// Each axis is an array of blocks, and each block is a 2D vector
+	// Each axis is an array of squares, and each square is a 2D vector
 	// where N is the length of the board (ie. the number of columns/rows to go through), the first N axes are vertical,
 	// the next N axes are horizontal, and the next two axes are top left to bottom right and top right to bottom left.
 	// AxisType provides a function to simplify determining which type of axis it is based on the index
-	val winAxes = Array(size * 2 + 2) { axisIndex ->
-		Array(size) { blockIndex ->
-			when (AxisType.getType(axisIndex, size)) {
-				AxisType.VERTICAL ->
-					intArrayOf(axisIndex % size, blockIndex)
-				AxisType.HORIZONTAL ->
-					intArrayOf(blockIndex, axisIndex % size)
-				AxisType.DESCENDING ->
-					intArrayOf(blockIndex, blockIndex)
-				AxisType.ASCENDING ->
-					intArrayOf(maxIndex - blockIndex, blockIndex)
-			}
-		}
-	}
 
 	enum class AxisType(val strikethroughChar: Char) {
 		VERTICAL('|'), HORIZONTAL('—'),
@@ -50,9 +40,54 @@ class Board(val size: Int) {
 		squares[row][column] = value
 	}
 
+	constructor(size: Int) :
+			this(size,
+
+				Array(size * 2 + 2) { axisIndex ->
+					Array(size) { squareIndex ->
+						when (AxisType.getType(axisIndex, size)) {
+							AxisType.VERTICAL ->
+								intArrayOf(axisIndex % size, squareIndex)
+							AxisType.HORIZONTAL ->
+								intArrayOf(squareIndex, axisIndex % size)
+							AxisType.DESCENDING ->
+								intArrayOf(squareIndex, squareIndex)
+							AxisType.ASCENDING ->
+								intArrayOf(size - 1 - squareIndex, squareIndex)
+						}
+					}
+				},
+
+				Array(size) { Array(size) { Symbol.EMPTY } })
+
+	constructor(board: Board) :
+			this(
+				board.size,
+				board.winAxes,
+				board.squares.map { row ->
+					row.clone()
+				}.toTypedArray()
+			)
+
+	fun getWinAxis(symbol: Symbol): Array<IntArray>? {
+		for (axis in winAxes) {
+			for (square in axis) {
+				if (this[square[0], square[1]] == symbol) {
+					if (axis.indexOf(square) == maxIndex) {
+						return axis
+					}
+				} else {
+					break
+				}
+			}
+		}
+		return null
+	}
+
 	// The function effectively checks the symbol against each winnable axis,
 	// and switches out every symbol in that axis to a strikethrough in string form until a full axis is found.
 	// then it returns the string it built. Alternatively, returns null if no win is found.
+	// TODO: check against winAxes first, get axis index (maybe in separate method), then start making string
 	fun checkForWin(symbol: Symbol): String? {
 		val boardString = toString()
 		for (axis in winAxes) {
@@ -63,12 +98,12 @@ class Board(val size: Int) {
 			}
 
 			// Uses an index to know when to stop and return the final string
-			for (blockIndex in axis.indices) {
-				val block = axis[blockIndex]
-				val column = block[0]
-				val row = block[1]
+			for (squareIndex in axis.indices) {
+				val square = axis[squareIndex]
+				val column = square[0]
+				val row = square[1]
 
-				if (this[block[0], block[1]] == symbol) {
+				if (this[square[0], square[1]] == symbol) {
 					// Two values to represent access to exact position in the editable board
 					val lineIndex = row * 2
 					val charIndex = column * 4 + 2
@@ -79,7 +114,7 @@ class Board(val size: Int) {
 
 					editableBoard[lineIndex][charIndex] = strikethrough
 
-					if (blockIndex == maxIndex) {
+					if (squareIndex == maxIndex) {
 						val finalBoard = StringBuilder()
 						for (line in editableBoard) {
 							finalBoard.append(line).append('\n')
@@ -96,6 +131,7 @@ class Board(val size: Int) {
 		return null
 	}
 
+	// TODO: perhaps avoid using squares val directly
 	override fun toString(): String {
 		val boardString = StringBuilder()
 		// Uses indices to avoid printing underline after last row
@@ -123,6 +159,7 @@ class Board(val size: Int) {
 		}
 		return boardString.toString()
 	}
+
 }
 
 fun main() {
@@ -135,4 +172,9 @@ fun main() {
 	println("Has O won: \n${board.checkForWin(Symbol.O)}")
 	println("Has X won: \n${board.checkForWin(Symbol.X)}")
 	println("Has EMPTY won: \n${board.checkForWin(Symbol.EMPTY)}")
+
+	val b2 = Board(board)
+	b2[0, 0] = Symbol.O
+	println("First board:\n$board")
+	println("Second board\n$b2")
 }
